5b50014a9f8364d5c2c37f1f16dd7c0a9110f4ad
David Golightly <davigoli@gmail.com>
2019-05-09 16:12:04 -0700

    divmod the minutes and hours

--- src/add.test.ts
+++ src/add.test.ts
@@ -1,10 +1,10 @@
 import { add } from "./add"
 import { DateTime } from "./parseISO"
 
-let dt1: DateTime
-let dt2: DateTime
+let dt: DateTime
+
 beforeEach(() => {
-  dt1 = {
+  dt = {
     year: 2013,
     month: 12,
     day: 2,
@@ -12,21 +12,10 @@ beforeEach(() => {
     minute: 0,
     second: 0
   }
-
-  dt2 = {
-    year: 0,
-    month: 0,
-    day: 0,
-    hour: 0,
-    minute: 0,
-    second: 0
-  }
 })
 
 it("adds two datetimes", () => {
-  dt2.day = 2
-
-  expect(add(dt1, dt2)).toEqual({
+  expect(add(dt, { day: 2 })).toEqual({
     year: 2013,
     month: 12,
     day: 4,
@@ -37,15 +26,38 @@ it("adds two datetimes", () => {
 })
 
 it("rolls over seconds to minutes", () => {
-  dt1.second = 45
-  dt2.second = 30
+  dt.second = 45
 
-  expect(add(dt1, dt2)).toEqual({
+  expect(add(dt, { second: 30 })).toEqual({
     year: 2013,
     month: 12,
     day: 2,
     hour: 0,
     minute: 1,
     second: 15
+  })
+})
+
+it("rolls over minutes to hours", () => {
+  dt.minute = 45
+
+  expect(add(dt, { minute: 30 })).toEqual({
+    year: 2013,
+    month: 12,
+    day: 2,
+    hour: 1,
+    minute: 15,
+    second: 0
+  })
+})
+
+it("rolls over hours to days", () => {
+  expect(add(dt, { hour: 33 })).toEqual({
+    year: 2013,
+    month: 12,
+    day: 3,
+    hour: 9,
+    minute: 0,
+    second: 0
   })
 })
--- src/add.ts
+++ src/add.ts
@@ -1,28 +1,30 @@
 import { DateTime } from "./parseISO"
 
-export const add = (base: DateTime, addend: DateTime): DateTime => {
-  const { minute: minuteRemainder, second } = addSecond(
-    base.second,
-    addend.second
+export const add = (base: DateTime, addend: Partial<DateTime>): DateTime => {
+  const [minuteRemainder, second] = divmod(base.second + (addend.second || 0))
+
+  const [hourRemainder, minute] = divmod(
+    base.minute + minuteRemainder + (addend.minute || 0)
+  )
+
+  const [dayRemainder, hour] = divmod(
+    base.hour + hourRemainder + (addend.hour || 0),
+    24
   )
 
   return {
-    year: base.year + addend.year,
-    month: base.month + addend.month,
-    day: base.day + addend.day,
-    hour: base.hour + addend.hour,
-    minute: base.minute + addend.minute + minuteRemainder,
+    year: base.year + (addend.year || 0),
+    month: base.month + (addend.month || 0),
+    day: base.day + (addend.day || 0) + dayRemainder,
+    hour,
+    minute,
     second
   }
 }
 
-const addSecond = (s1: number, s2: number) => {
-  const newSeconds = s1 + s2
-  const minute = Math.floor(newSeconds / 60)
-  const second = newSeconds % 60
+const divmod = (s: number, base: number = 60) => {
+  const bigHand = Math.floor(s / base)
+  const littleHand = s % base
 
-  return {
-    minute,
-    second
-  }
+  return [bigHand, littleHand]
 }
