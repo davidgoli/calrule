6cf2935745269052401d59e212c3e48f25fb9d4a
David Golightly <davigoli@gmail.com>
2019-05-11 16:16:03 -0700

    valid with dtstart and freq

--- src/validate.test.ts
+++ src/validate.test.ts
@@ -4,6 +4,22 @@ it('is not valid if there is no freq', () => {
   // @ts-ignore
   expect(validate({})).toEqual([
     false,
-    { errors: [`Invalid value "undefined" for paramater FREQ`] }
+    {
+      errors: [
+        `Invalid value "undefined" for parameter FREQ`,
+        `Invalid value "undefined" for parameter DTSTART`
+      ]
+    }
   ])
+})
+
+it('is not valid if dtstart does not parse', () => {
+  expect(validate({ freq: 'DAILY', dtstart: '' })).toEqual([
+    false,
+    { errors: ['Invalid value "" for parameter DTSTART'] }
+  ])
+})
+
+it('is valid with valid FREQ and DTSTART', () => {
+  expect(validate({ freq: 'DAILY', dtstart: '2017-04-15' })).toEqual([true, {}])
 })
--- src/validate.ts
+++ src/validate.ts
@@ -1,4 +1,5 @@
 import { RuleOptions, Frequency } from './types'
+import { parseISO } from './DateTime/parseISO'
 
 const FREQValues: Frequency[] = [
   'YEARLY',
@@ -10,12 +11,21 @@ const FREQValues: Frequency[] = [
   'SECONDLY'
 ]
 
+const error = (param: string, value: any) =>
+  `Invalid value "${value}" for parameter ${param}`
+
 export const validate = (options: RuleOptions) => {
+  const errors = []
   if (FREQValues.indexOf(options.freq) === -1) {
-    return [
-      false,
-      { errors: [`Invalid value "${options.freq}" for paramater FREQ`] }
-    ]
+    errors.push(error('FREQ', options.freq))
+  }
+
+  if (!parseISO(options.dtstart)) {
+    errors.push(error('DTSTART', options.dtstart))
+  }
+
+  if (errors.length) {
+    return [false, { errors }]
   }
 
   return [true, {}]
