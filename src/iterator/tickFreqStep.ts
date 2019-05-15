b77ccf9c6c66d27da3fabaea20ebb5b385fa5c73
David Golightly <davigoli@gmail.com>
2019-05-14 18:40:19 -0700

    get unstuck when freq unit has a byrule

--- src/iterator/tickFreqStep.ts
+++ src/iterator/tickFreqStep.ts
@@ -4,20 +4,29 @@ import { FREQUENCY_ORDER, smallestTickUnit, byRuleForUnit } from './units'
 import { add } from '../DateTime/add'
 import { firstWeekdayOfMonth } from '../DateTime/dayOfWeek'
 import { Weekday } from '../types'
+import { tickByrule } from './tickByrule'
+import { compare } from '../DateTime/compare'
 
 export const tickFreqStep = (
   current: DateTime,
   unit: keyof DateTime,
   options: GroomedOptions
 ) => {
-  const next = add(current, {
+  let next = add(current, {
     [unit]: (options.interval || 1) * (options.freq === 'WEEKLY' ? 7 : 1)
   })
 
+  console.log({ next })
   const byrule = byRuleForUnit(unit, options)
   if (byrule) {
-    next[unit] = nextValueForUnit(unit, options, next)
+    next = tickByrule(next, unit, options)
+    if (compare(next, current) === 0) {
+      const higherUnit = FREQUENCY_ORDER[FREQUENCY_ORDER.indexOf(unit) - 1]
+      next = tickFreqStep(current, higherUnit, options)
+    }
   }
+
+  console.log({ byrule: next[unit] })
 
   const unitIdx = FREQUENCY_ORDER.indexOf(unit)
   FREQUENCY_ORDER.slice(
