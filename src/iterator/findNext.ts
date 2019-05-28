eadebf1be929b8e2128422c7bb7e2234def83a81
David Golightly <davigoli@gmail.com>
2019-05-28 13:34:17 -0700

    refactor for clarity

--- src/iterator/findNext.ts
+++ src/iterator/findNext.ts
@@ -5,23 +5,6 @@ import { GroomedOptions } from '../groomOptions'
 import { syncWithRule } from './syncWithRule'
 import { FREQUENCY_COUNTER, FREQUENCY_ORDER } from './units'
 
-// 2 main operations: advance and sync
-
-// advance:
-// 1. Find smallest unit based on byX + freq
-// 2a. if unit == freq: increment by interval
-// 2b. else: increment by 1
-// 3. initialize all smaller units to 1 or 0
-// 4. sync with rule options
-
-// sync:
-// 1. for each second, minute, hour, day, month, year starting from smallest unit:
-//   a. if it is < byX:
-//     i. increase to byX
-//     ii. done
-//   b. else:
-//     i. set to byX[0]
-//     ii. move to the next bigger unit
 export const findNext = (initial: DateTime, options: GroomedOptions) => {
   let unitIdx = FREQUENCY_ORDER.length - 1
 
@@ -32,12 +15,10 @@ export const findNext = (initial: DateTime, options: GroomedOptions) => {
 
   do {
     const unit = FREQUENCY_ORDER[unitIdx]
-    const base =
-      unit === 'day' && options.freq === 'WEEKLY' && !options.byday ? 7 : 1
     const interval =
       unit === FREQUENCY_COUNTER[options.freq] ? options.interval : 1
 
-    next = add(next, { [unit]: base * interval })
+    next = add(next, { [unit]: interval })
     next = syncWithRule(next, options)
   } while (compare(initial, next) >= 0 && --unitIdx >= 0)
 
--- src/iterator/syncWithRule.ts
+++ src/iterator/syncWithRule.ts
@@ -1,60 +1,78 @@
 import { copy } from '../copy'
-import { DateTime, DateTimeDiff } from '../DateTime'
+import { DateTime } from '../DateTime'
 import { add } from '../DateTime/add'
 import { compare } from '../DateTime/compare'
 import { dayOfWeek, dayOrdinalOfWeek } from '../DateTime/units'
 import { GroomedOptions } from '../groomOptions'
-import { initializeFrom } from './initializeFrom'
+import { initializeSmallerUnits } from './initializeSmallerUnits'
 import { shouldRollOver } from './shouldRollOver'
 import { syncByrule } from './syncByrule'
 import { byRuleForUnit, FREQUENCY_COUNTER, FREQUENCY_ORDER } from './units'
 
+const strideToRollover = (
+  next: DateTime,
+  unit: keyof DateTime,
+  options: GroomedOptions
+) => {
+  const lastByday = options.byday && options.byday[options.byday.length - 1]
+  const wday = dayOfWeek(next)
+
+  const weeklyStride =
+    unit === 'day' &&
+    options.freq === 'WEEKLY' &&
+    (!options.byday || (wday === lastByday && options.interval > 1))
+
+  const base = weeklyStride ? 7 : 1
+  const interval =
+    unit === FREQUENCY_COUNTER[options.freq] ? options.interval : 1
+
+  return base * interval - (weeklyStride ? dayOrdinalOfWeek(next) : 0)
+}
+
+const advanceUnit = (
+  next: DateTime,
+  unit: keyof DateTime,
+  options: GroomedOptions
+) =>
+  add(next, {
+    [unit]: strideToRollover(next, unit, options)
+  })
+
+const diffToNextByruleValue = (
+  next: DateTime,
+  unit: keyof DateTime,
+  options: GroomedOptions
+) => {
+  const byrule = byRuleForUnit(unit, options)
+  return byrule ? syncByrule(next, byrule) : {}
+}
+
+const nextHigherUnit = (unit: keyof DateTime) =>
+  FREQUENCY_ORDER[FREQUENCY_ORDER.indexOf(unit) - 1]
+
 export const syncWithRule = (initial: DateTime, options: GroomedOptions) => {
   let next = copy(initial)
-  let toRollOver: keyof DateTime | 'freq' | undefined
+  let unit: keyof DateTime | undefined
 
-  do {
-    toRollOver = shouldRollOver(next, options)
-    if (!toRollOver) {
-      break
-    }
-
-    let diff: DateTimeDiff
-    let diffValue: number | undefined = 0
+  while ((unit = shouldRollOver(next, options))) {
+    let unitDiff: number | undefined = 0
 
     do {
-      const lastByday = options.byday && options.byday[options.byday.length - 1]
-      const wday = dayOfWeek(next)
+      next = advanceUnit(next, unit, options)
+      next = initializeSmallerUnits(next, unit)
 
-      const weeklyStride =
-        toRollOver === 'day' &&
-        options.freq === 'WEEKLY' &&
-        (!options.byday || (wday === lastByday && options.interval > 1))
-
-      const base = weeklyStride ? 7 : 1
-      const interval =
-        toRollOver === FREQUENCY_COUNTER[options.freq] ? options.interval : 1
-
-      const stride =
-        base * interval - (weeklyStride ? dayOrdinalOfWeek(next) : 0)
-      const byrule1 = byRuleForUnit(toRollOver, options)
-      diff = byrule1 ? syncByrule(next, byrule1) : {}
-
-      next = add(next, { [toRollOver]: stride })
-
-      const byrule = byRuleForUnit(toRollOver, options)
-      diff = byrule ? syncByrule(next, byrule) : {}
-      next = initializeFrom(next, toRollOver)
+      const diff = diffToNextByruleValue(next, unit, options)
 
       next = add(next, diff)
-      diffValue = diff[toRollOver]
-      if (typeof diffValue === 'number' && diffValue < 0) {
-        toRollOver = FREQUENCY_ORDER[FREQUENCY_ORDER.indexOf(toRollOver) - 1]
-      }
-    } while (typeof diffValue === 'number' && diffValue < 0)
+      unitDiff = diff[unit]
 
-    toRollOver = shouldRollOver(next, options)
-  } while (compare(initial, next) >= 0 || toRollOver)
+      unit = nextHigherUnit(unit)
+    } while (unitDiff && unitDiff < 0)
+
+    if (compare(initial, next) >= 0) {
+      break
+    }
+  }
 
   return next
 }
