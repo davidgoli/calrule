74486b0d982421ee3c2f30c33d5c054f834b36fc
David Golightly <davigoli@gmail.com>
2019-05-28 17:26:57 -0700

    streamline findNext

--- src/iterator/findNext.ts
+++ src/iterator/findNext.ts
@@ -3,22 +3,16 @@ import { add } from '../DateTime/add'
 import { compare } from '../DateTime/compare'
 import { GroomedOptions } from '../groomOptions'
 import { syncWithRule } from './syncWithRule'
-import { FREQUENCY_COUNTER, FREQUENCY_ORDER } from './units'
 
 export const findNext = (initial: DateTime, options: GroomedOptions) => {
-  let unitIdx = FREQUENCY_ORDER.length - 1
-
-  let next: DateTime = syncWithRule(initial, options)
+  let next = syncWithRule(initial, options)
   if (compare(initial, next) < 0) {
     return next
   }
 
-  const unit = FREQUENCY_ORDER[unitIdx]
-  const interval =
-    unit === FREQUENCY_COUNTER[options.freq] ? options.interval : 1
+  next = add(next, {
+    second: options.freq === 'SECONDLY' ? options.interval : 1
+  })
 
-  next = add(next, { [unit]: interval })
-  next = syncWithRule(next, options)
-
-  return next
+  return syncWithRule(next, options)
 }
